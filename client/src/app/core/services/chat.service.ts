033dbe457a76a58d16f6837b569290d9e5b14721
Ilya <rodnov.ilya777@gmail.com>
2026-03-23 15:31:13 +0300

    feat(chat): создание сервиса для чатов

--- client/src/app/core/services/chat.service.ts
+++ client/src/app/core/services/chat.service.ts
@@ -1,11 +1,26 @@
 import { Injectable } from "@angular/core";
-import { HttpClient } from "@angular/common/http";
+import { ApiService } from './api.service';
 import { Observable } from "rxjs";
-import { Chat } from "../../models/chat.model";
+import { Chat, Message } from "../../models/chat.model";
 
 @Injectable({providedIn: 'root'})
 export class ChatService{
     private baseUrl = 'http://localhost:3000/api';
-    constructor(private http: HttpClient) {}
+    constructor(private apiService: ApiService) {}
 
+    getChats(): Observable<Chat[]> {
+        return this.apiService.getChats()
+    }
+
+    createDirect(targetUserId: string): Observable<Chat> {
+        return this.apiService.createDirect(targetUserId);
+    }
+
+    createGroup(data:{name: string, memberIds: string[]}): Observable<Chat>{
+        return this.apiService.createGroup(data);
+    }
+
+    getMessages(id: Chat["id"]): Observable<Message[]> {
+        return this.apiService.getMessages(id);
+    }
 }
