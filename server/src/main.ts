5d500e94ab11cdeecfeb797afe3230bb06635ec5
Ilya <rodnov.ilya777@gmail.com>
2026-04-15 12:38:42 +0300

    feat(server): подключение redis-адаптера

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,7 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
-import * as express from 'express';
+import { IoAdapter } from '@nestjs/platform-socket.io';
+import { createClient } from 'redis';
+import { createAdapter } from '@socket.io/redis-adapter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -10,6 +12,19 @@ async function bootstrap() {
   app.setGlobalPrefix('api');
   app.enableCors();
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  const pubClient = createClient({ url: 'redis://localhost:6379' });
+  const subClient = pubClient.duplicate();
+
+  await Promise.all([pubClient.connect(), subClient.connect()]);
+
+  app.useWebSocketAdapter(new (class extends IoAdapter {
+    createIOServer(port: number, options?: any): any {
+      const server = super.createIOServer(port, options);
+      server.adapter(createAdapter(pubClient, subClient));
+      return server;
+    }
+  })(app));
 
   const config = new DocumentBuilder()
     .setTitle('Server')
